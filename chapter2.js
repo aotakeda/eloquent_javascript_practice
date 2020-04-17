9616a4a6cd879471ed738ac2d0172f0fdee88871
Arthur Takeda <arthur.ot@hotmail.com>
2020-04-17 20:17:51 -0300

    chapter 2: first exercise done

---
+++ chapter2.js
@@ -0,0 +1,15 @@
+/* Write a loop that makes seven calls to console.log to output the following triangle:
+
+_________________________
+#
+##
+###
+####
+#####
+######
+__________________________
+*/
+
+for (let index = 1; index < 7; index++) {
+    console.log('#'.repeat(index))
+}
