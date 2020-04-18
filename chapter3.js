a74af95a3e3e3f68ea88b493946e6286252a9e16
Arthur Takeda <arthur.ot@hotmail.com>
2020-04-18 11:26:15 -0300

    chapter 3: first exercise done

---
+++ chapter3.js
@@ -0,0 +1,16 @@
+/*
+
+Write a function min that takes two arguments and returns their
+minimum.
+
+*/
+
+function min(firstNumber, secondNumber) {
+    if (firstNumber < secondNumber) {
+        return firstNumber;
+    }
+    else {
+        return secondNumber;
+    }
+}
+
