48ec937387779343e894322ed18a44c9e3517369
Arthur Takeda <arthur.ot@hotmail.com>
2020-04-18 21:05:32 -0300

    chapter 4: second exercise done

--- chapter4.js
+++ chapter4.js
@@ -47,5 +47,41 @@ function rangeBonus(start, end, step = 1) {
             arr.push(end); 
         }
     }
-    console.log(arr);
+    return arr;
 }
+
+/*
+
+Arrays have a reverse method that changes the array by inverting the
+order in which its elements appear. For this exercise, write two functions,
+reverseArray and reverseArrayInPlace.
+
+The first, reverseArray, takes an array
+as argument and produces a new array that has the same elements in the
+inverse order.
+
+The second, reverseArrayInPlace, does what the reverse
+method does: it modifies the array given as argument by reversing its ele-
+ments. Neither may use the standard reverse method.
+
+*/
+
+array = [1,2,3,4,5,6]
+
+function reverseArray(arr) {
+    var newArray = new Array;
+    for (let index = 0; index < arr.length; index++) {
+        newArray.unshift(arr[index])
+    }
+    return newArray;
+}
+
+function reverseArrayInPlace(arr) {
+    var newArray = new Array;
+    for(var i = arr.length-1; i >= 0; i--) {
+        newArray.push(arr[i]);
+    }
+    return newArray;
+}
+
+array = reverseArrayInPlace(array);
